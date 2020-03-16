4237fe39adfb1fd205574ed052a164cd87c6575a
745491146@qq.com <745491146@qq.com>
2020-03-16 11:03:45 +0800

    提交<7.整数反转>

---
+++ src/algorithm/java/easy/reverse/Solution.java
@@ -0,0 +1,29 @@
+package algorithm.java.easy.reverse;
+
+/**
+ * 7.整数反转
+ */
+public class Solution {
+
+  public static void main(String[] args) {
+
+    int x = 123;
+    System.out.println(reverse01(x));
+
+  }
+
+  private static int reverse01(int x) {
+    //反转后的数
+    long res = 0;
+
+    while (x != 0) {
+      res = res * 10 + x % 10;
+      x /= 10;
+    }
+
+    //return (int) res == res ? (int) res : 0;
+    //or
+    return (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) ? 0 : (int) res;
+  }
+
+}
